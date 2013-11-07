318d79d381c3c70516bb371b49b70f920d48992f
daniel-bryant <bryant.daniel.j@gmail.com>
2013-11-06 23:03:40 -0500

    testing

--- addphotos.php
+++ addphotos.php
@@ -1,52 +1,26 @@
 <!-- PHP Mongo Docs: http://php.net/manual/en/class.mongodb.php -->
 <html>
 <body>
-<h1>MongoHQ Test</h1>
+<h1>Add Photos</h1>
 <?php
   try {
     // connect to MongoHQ assuming your MONGOHQ_URL environment
     // variable contains the connection string
     $connection_url = getenv("MONGOHQ_URL");
-    echo "<p>here</p>";
+
     // create the mongo connection object
     $m = new Mongo($connection_url);
-    echo "<p>here 2</p>";
+
     // extract the DB name from the connection path
     $url = parse_url($connection_url);
     $db_name = preg_replace('/\/(.*)/', '$1', $url['path']);
-    echo "<p>here 3</p>";
+
     // use the database we connected to
     $db = $m->selectDB($db_name);
     //get GridFS
     $grid = $db->getGridFS();
 
-    echo "<h2>Collections</h2>";
-    echo "<ul>";
 
-    // print out list of collections
-    $cursor = $db->listCollections();
-    $collection_name = "";
-    foreach( $cursor as $doc ) {
-      echo "<li>" . $doc->getName() . "</li>";
-      $collection_name = $doc->getName();
-    }
-    echo "</ul>";
-
-    // print out last collection
-    if ( $collection_name != "" ) {
-      $collection = $db->selectCollection($collection_name);
-      echo "<h2>Documents in ${collection_name}</h2>";
-
-      // only print out the first 5 docs
-      $cursor = $collection->find();
-      $cursor->limit(5);
-      echo $cursor->count() . ' document(s) found. <br/>';
-      foreach( $cursor as $doc ) {
-        echo "<pre>";
-        var_dump($doc);
-        echo "</pre>";
-      }
-    }
 
     // disconnect from server
     $m->close();
