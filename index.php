b79383fb9c97bd2267e31c206a61dfa229183a27
daniel-bryant <bryant.daniel.j@gmail.com>
2013-11-07 23:16:34 -0500

    home page update

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-  phpinfo();
-?>
+<html>
+ <head>
+  <title>PHP Image Service</title>
+ </head>
+ <body>
+  <h1>PHP Image Service Home</h1>
+  <p>
+    This service serves as a portal through which a MongoDB database,
+    containing a web applications images, may be accessed. To see an
+    example, please visit 
+    <a href="http://sheltered-brook-1332.herokuapp.com/albums">sheltered-brook-1332.herokuapp.com/albums</a>
+    . Written in pure PHP, by Daniel Bryant.
+  </p>
+  <p>&copy; Daniel Bryant</p>
+ </body>
+</html>
